466958c8424465ac19278f1a66e30439e0498bba
Pablo Ossandón <pabloossandon88@gmail.com>
2024-03-14 13:37:37 -0300

    limpie rutas que ya no utilizo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-//import { DialogComponent } from './dialog/dialog.component';
 
 export const routes: Routes = [
-    {path: 'dashboard', component: DashboardComponent}
+    {path: 'dashboard', component: DashboardComponent},
     {path: '', component: DashboardComponent}
-   // {path: 'dialog', component: DialogComponent }
 ];
